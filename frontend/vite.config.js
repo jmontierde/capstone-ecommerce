dd9a29a7b0b8e5c23d737c1d2cfb19f0eaa1c449
Jayson Caburatan <montierdejayson@gmail.com>
2023-12-01 08:37:48 +0800

    buildv11

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,22 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
+  plugins: [react()],
   build: {
-    rollupOptions: {
-      external: [
-        // Existing external dependencies
-        /^node:/,
-
-        // Add react-alert as an external dependency
-        "react-alert",
-        "react-alert-template-basic",
-        "react-input-emoji",
-        "react-messenger-customer-chat",
-        "use-react-countries",
-      ],
-    },
     chunkSizeWarningLimit: 100000000,
   },
 });
